83799b6f7960e33a0b66aee864b4b549f37e9271
Tony Dzines <tony@dzines.co.uk>
2024-10-27 22:28:12 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,10 @@ function App() {
           <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
             <ErrorBoundary>
               <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/blog/:slug" element={<BlogPost />} />
-                <Route path="/search" element={<SearchPage />} />
-              </Routes>
+  <Route path="/" element={<HomePage />} />
+  <Route path="/dog-walks/:slug" element={<BlogPost />} />  {/* Changed from /blog to /dog-walks */}
+  <Route path="/search" element={<SearchPage />} />
+</Routes>
             </ErrorBoundary>
           </main>
           <Footer />
