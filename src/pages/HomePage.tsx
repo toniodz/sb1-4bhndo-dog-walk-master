f7fd9ae37d98d7b16ec6725558ec3fa95b1b8274
Tony Dzines <tony@dzines.co.uk>
2024-11-02 14:04:09 +0000

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -97,7 +97,7 @@ const HomePage: React.FC = () => {
            </div>
          )}
          
-         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
            {walks.map((walk) => (
              <Link 
   key={walk.id} 
